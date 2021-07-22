62728597c281956599f6b2a7fbbdb8c5e582e2dc
adriansteger <asco54@gmail.com>
2021-07-22 16:50:56 +0200

    added firebase and axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Netflix Clone</h1>
     </div>
   );
 }
 
 export default App;
+
+
+
+
+//e466c0c5264b05abd29c1fee45edd288
+//https://netflix-clone-aa889.web.app/
---
+++ src/requests.js
@@ -0,0 +1,1 @@
+const API_KEY = "e466c0c5264b05abd29c1fee45edd288";
