57583930254629e24353a81085de420e858273df
adriansteger <asco54@gmail.com>
2021-07-22 18:22:58 +0200

    created base requests and rows with images

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
+import requests from './requests';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
     </div>
   );
 }
 
 export default App;
 
-
-
-
-//e466c0c5264b05abd29c1fee45edd288
 //https://netflix-clone-aa889.web.app/
