bdd8d81c6d00d8a3d9d17629ae48a6b89b334ec9
adriansteger <asco54@gmail.com>
2021-07-22 17:07:44 +0200

    added requests

--- src/requests.js
+++ src/requests.js
@@ -1,1 +1,14 @@
 const API_KEY = "e466c0c5264b05abd29c1fee45edd288";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=de-DE`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=123`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=de-DE`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
