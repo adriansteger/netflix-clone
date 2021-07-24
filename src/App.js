f946eec364c547514a66d5689bbc1b213ff383ba
adriansteger <asco54@gmail.com>
2021-07-24 23:56:42 +0200

    full build

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import './App.css';
-import requests from './requests';
-import Row from './Row';
+import React from "react";
+import "./App.css";
+import requests from "./requests";
+import Row from "./Row";
+import Banner from "./Banner";
+import "./App.css";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
